c68eeba9125d7483871b67747630a4a2996d087a
Tên GitHub của bạn <Email GitHub ca bn>
2025-05-25 07:44:41 +0700

    cnlayoout2

--- client/src/components/interface1/MediaArea.tsx
+++ client/src/components/interface1/MediaArea.tsx
@@ -2,3 +2,6 @@ import React from 'react';
 // ... existing code ...
 // MediaArea component sẽ được copy logic từ phần ServiceCard và media display trong Interface1.tsx
 // ... existing code ... 
+
+const MediaArea = () => null;
+export default MediaArea; 
